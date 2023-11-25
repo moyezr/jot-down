e5ba9869dc334c277cb2774c4814283780c6408d
Moyez <moyezrabbani@gmail.com>
2023-11-26 00:27:35 +0530

    added sidebar

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const DocumentsPage = (props: Props) => {
+  return (
+    <div>
+
+        This is a protected route
+        
+    </div>
+  )
+}
+
+export default DocumentsPage

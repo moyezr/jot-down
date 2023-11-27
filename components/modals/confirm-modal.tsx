5f435e6f0b1c29e6dd47023c0d82754ae8895235
Moyez <moyezrabbani@gmail.com>
2023-11-27 13:09:34 +0530

    Added Deleting feature, archiving feature, confirm modal for deleting documents

---
+++ components/modals/confirm-modal.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { ReactNode } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+interface ConfirmModalProps {
+  children: ReactNode;
+  onConfirm: () => void;
+}
+
+const ConfirmModal = ({ children, onConfirm }: ConfirmModalProps) => {
+
+    const handleConfirm = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.stopPropagation();
+        onConfirm();
+    }
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger onClick={(e: any) => e.stopPropagation()} asChild>
+        {children}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={(e: any) => e.stopPropagation()}>
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={handleConfirm}>Confirm</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default ConfirmModal;
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -5,9 +5,11 @@ import {
   ChevronLeft,
   ChevronsLeft,
   MenuIcon,
+  Plus,
   PlusCircle,
   Search,
   Settings,
+  Trash,
 } from "lucide-react";
 import { useParams, usePathname, useRouter } from "next/navigation";
 import { ElementRef, MouseEvent, useEffect, useRef, useState } from "react";
@@ -18,7 +20,12 @@ import Item from "./item";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
 import DocumentList from "./document-list";
-
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import TrashBox from "./trash-box";
 type Props = {};
 
 const Navigation = (props: Props) => {
@@ -151,6 +158,15 @@ const Navigation = (props: Props) => {
         </div>
         <div className="mt-4">
           <DocumentList />
+          <Item onClick={handleCreate} label="Add a page" icon={Plus} />
+          <Popover>
+            <PopoverTrigger className="w-full mt-4">
+              <Item label="Trash" icon={Trash} />
+            </PopoverTrigger>
+            <PopoverContent className="p-0 w-72" side={isMobile ? "bottom" : "right"}>
+              <TrashBox />
+            </PopoverContent>
+          </Popover>
         </div>
 
         <div
